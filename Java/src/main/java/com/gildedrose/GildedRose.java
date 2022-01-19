2770b3b222ce1625b7563447efe3fbe8fd50f678
Sartain, Alex <alex.sartain@zuhlke.com>
2022-01-19 14:31:24 +0000

    Isolate aged brie logic

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -35,6 +35,12 @@ class GildedRose {
         }
     }
 
+    public void updateAgedBrie(int i) {
+        if (items[i].sellIn < 0) {
+            increaseQualityIfQualityLessThan50(i);
+        }
+    }
+
     public void increaseQualityIfQualityLessThan50(int i) {
         if (items[i].quality < 50) {
             items[i].quality = items[i].quality + 1;
@@ -47,19 +53,16 @@ class GildedRose {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 decreaseQuality(i);
+                if (items[i].sellIn < 0) {
+                    decreaseQuality(i);
+                }
             } else {
                 increaseQualityIfQualityLessThan50(i);
                 if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                     updateBackstagePass(i);
                 }
-            }
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        decreaseQuality(i);
-                    }
-                } else {
-                    increaseQualityIfQualityLessThan50(i);
+                if(items[i].name.equals("Aged Brie")) {
+                    updateAgedBrie(i);
                 }
             }
         }
