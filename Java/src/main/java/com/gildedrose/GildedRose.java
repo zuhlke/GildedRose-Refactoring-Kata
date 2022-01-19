e9aaa4d2f4c30c639fed9ddcd0b7f7ea7097eb8b
Sartain, Alex <alex.sartain@zuhlke.com>
2022-01-19 14:12:23 +0000

    Separate sell in decrease into method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -15,6 +15,12 @@ class GildedRose {
         }
     }
 
+    public void decreaseSellIn(int i) {
+        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            items[i].sellIn = items[i].sellIn - 1;
+        }
+    }
+
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
@@ -39,11 +45,7 @@ class GildedRose {
                     }
                 }
             }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
+            decreaseSellIn(i);
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
