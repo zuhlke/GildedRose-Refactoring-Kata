e8681c026975a1418eef81f138f1d808d75f3332
acridley <91603689+acridley@users.noreply.github.com>
2022-01-19 14:37:58 +0000

    Refactoring by extracting code into individual methods

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -5,22 +5,13 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def update_quality(self):
+    def update_quality_sellin(self):
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros":
-                item.quality = item.quality - 1
+                self.update_quality_forStandardItem(item)
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                self.update_quality_forBrieAndBackstagePasses(item)
+            self.update_sellin(item)
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
@@ -32,6 +23,22 @@ class GildedRose(object):
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+
+    def update_quality_forBrieAndBackstagePasses(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.sell_in < 11 and item.quality < 50:
+                    item.quality = item.quality + 1
+                if item.sell_in < 6 and item.quality < 50:
+                    item.quality = item.quality + 1
+
+    def update_quality_forStandardItem(self, item):
+        item.quality = item.quality - 1
+
+    def update_sellin(self, item):
+        if item.name != "Sulfuras, Hand of Ragnaros":
+            item.sell_in = item.sell_in - 1
 
 
 class Item:
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -9,23 +9,23 @@ class GildedRoseTest(unittest.TestCase):
     def test_standard_item(self):
         item = Item(name="+5 Dexterity Vest", sell_in=10, quality=20)
         gilded_rose = GildedRose([item])
-        gilded_rose.update_quality()
+        gilded_rose.update_quality_sellin()
         self.assertEquals(item.quality, 19)
         self.assertEquals(item.sell_in, 9)
 
     def test_brie_item_quality_goes_up(self):
         item =  Item(name="Aged Brie", sell_in=2, quality=0)
         gilded_rose = GildedRose([item])
-        gilded_rose.update_quality()
+        gilded_rose.update_quality_sellin()
         self.assertEquals(item.quality, 1)
         self.assertEquals(item.sell_in, 1)
 
     def test_brie_item_quality_goes_up(self):
         item =  Item(name="Elixir of the Mongoose", sell_in=5, quality=7)
         gilded_rose = GildedRose([item])
-        gilded_rose.update_quality()
-        self.assertEquals(item.quality, 1)
-        self.assertEquals(item.sell_in, 1)
+        gilded_rose.update_quality_sellin()
+        self.assertEquals(item.quality, 6)
+        self.assertEquals(item.sell_in, 4)
         
 if __name__ == '__main__':
     unittest.main()
