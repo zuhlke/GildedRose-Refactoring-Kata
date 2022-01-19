a0893cce486f66fc67111afd7d30df6985e06215
Sartain, Alex <alex.sartain@zuhlke.com>
2022-01-19 13:48:58 +0000

    Add test for backstagePass quality increasing as sellin approaches

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -72,19 +72,30 @@ class GildedRoseTest {
     }
 
     @Test
-    void sellNOfSulfurasCannotDecreaseOrIncrease() {
+    void sellInOfSulfurasCannotDecreaseOrIncrease() {
         Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 20, 49) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals(20, app.items[0].sellIn);
     }
 
+    @Test
+    void backstagePassIncreaseInQualityWhenSellInApproachesZero() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 20, 30),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 30),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 30)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(31, app.items[0].quality);
+        assertEquals(32, app.items[1].quality);
+        assertEquals(33, app.items[2].quality);
+    }
     /*
     Once the sell by date has passed, Quality degrades twice as fast = Done
     The Quality of an item is never negative = Done
     "Aged Brie" actually increases in Quality the older it gets = Done
     The Quality of an item is never more than 50 = Done
-    "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+    "Sulfuras", being a legendary item, never has to be sold or decreases in Quality = Done
     "Backstage passes", like aged brie, increases in Quality as it's SellIn value approaches;
     Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
      */
