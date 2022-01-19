784cf892bff5e1fa02055baed7879ed45287146a
Sartain, Alex <alex.sartain@zuhlke.com>
2022-01-19 14:09:37 +0000

    Refactor decrease quality method

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -7,15 +7,19 @@ class GildedRose {
         this.items = items;
     }
 
+    public void decreaseQuality(int i) {
+        if (items[i].quality > 0) {
+            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                items[i].quality = items[i].quality - 1;
+            }
+        }
+    }
+
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+                decreaseQuality(i);
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
@@ -43,11 +47,7 @@ class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
+                        decreaseQuality(i);
                     } else {
                         items[i].quality = items[i].quality - items[i].quality;
                     }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -106,6 +106,6 @@ class GildedRoseTest {
     "Sulfuras", being a legendary item, never has to be sold or decreases in Quality = Done
     "Backstage passes", like aged brie, increases in Quality as it's SellIn value approaches;
     Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
-     */
+    */
 
 }
