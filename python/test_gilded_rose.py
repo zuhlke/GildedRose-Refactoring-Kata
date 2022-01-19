32a16245fa6167942c13e02a0f8ac4135db9a4d1
acridley <91603689+acridley@users.noreply.github.com>
2022-01-19 14:16:34 +0000

    Starting point with a few added tests and one thing changed in gilded_rose.py

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -5,12 +5,27 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
 
+    def test_standard_item(self):
+        item = Item(name="+5 Dexterity Vest", sell_in=10, quality=20)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertEquals(item.quality, 19)
+        self.assertEquals(item.sell_in, 9)
+
+    def test_brie_item_quality_goes_up(self):
+        item =  Item(name="Aged Brie", sell_in=2, quality=0)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertEquals(item.quality, 1)
+        self.assertEquals(item.sell_in, 1)
+
+    def test_brie_item_quality_goes_up(self):
+        item =  Item(name="Elixir of the Mongoose", sell_in=5, quality=7)
+        gilded_rose = GildedRose([item])
+        gilded_rose.update_quality()
+        self.assertEquals(item.quality, 1)
+        self.assertEquals(item.sell_in, 1)
         
 if __name__ == '__main__':
     unittest.main()
