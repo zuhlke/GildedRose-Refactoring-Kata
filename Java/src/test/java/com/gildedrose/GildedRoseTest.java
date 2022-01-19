d1ac7b2cef23cb948c2eab9031112113ec2ce3bb
Sartain, Alex <alex.sartain@zuhlke.com>
2022-01-19 13:33:17 +0000

    Add test for doubling quality degradation after sellby passes

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,26 @@ class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void sellByDatePassedQualityDegradesTwiceAsFast() {
+        Item[] items = new Item[] { new Item("foo", 1, 20) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        int qualityExpected = 19;
+        assertEquals(qualityExpected, app.items[0].quality);
+        app.updateQuality();
+        qualityExpected = 17;
+        assertEquals(qualityExpected, app.items[0].quality);
+    }
+
+    /*
+    Once the sell by date has passed, Quality degrades twice as fast
+The Quality of an item is never negative
+"Aged Brie" actually increases in Quality the older it gets
+The Quality of an item is never more than 50
+"Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+"Backstage passes", like aged brie, increases in Quality as it's SellIn value approaches;
+Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but Quality drops to 0 after the concert
+     */
+
 }
