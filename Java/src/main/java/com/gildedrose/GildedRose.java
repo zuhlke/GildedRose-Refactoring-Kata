43061bd48b39e679dedccbefe204417c3635693c
Sartain, Alex <alex.sartain@zuhlke.com>
2022-01-19 14:21:27 +0000

    Separate backstage pass logic

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -21,8 +21,27 @@ class GildedRose {
         }
     }
 
+    public void updateBackstagePass(int i) {
+        if (items[i].sellIn < 0) {
+            items[i].quality = items[i].quality - items[i].quality;
+        }
+        else {
+            if (items[i].sellIn < 11) {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
+            }
+            if (items[i].sellIn < 6) {
+                if (items[i].quality < 50) {
+                    items[i].quality = items[i].quality + 1;
+                }
+            }
+        }
+    }
+
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
+            decreaseSellIn(i);
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 decreaseQuality(i);
@@ -31,27 +50,14 @@ class GildedRose {
                     items[i].quality = items[i].quality + 1;
 
                     if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+                        updateBackstagePass(i);
                     }
                 }
             }
-            decreaseSellIn(i);
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         decreaseQuality(i);
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
                     }
                 } else {
                     if (items[i].quality < 50) {
